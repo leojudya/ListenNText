c9fe0103bb9b2e723d34f61d0b3a652155dbe91f
Lin Zi Yuan <leojudya@gmail.com>
2024-03-13 20:16:35 +0800

    play rate

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ document.addEventListener("DOMContentLoaded", () => {
   const audio = document.getElementById("audio-player");
   const textarea = document.getElementById("input-area");
   const uploadBtn = document.getElementById('upload-button');
+  const playRate = document.getElementById('play-rate');
 
   textarea.focus();
 
@@ -39,10 +40,15 @@ document.addEventListener("DOMContentLoaded", () => {
             const reader = new FileReader(); // 創建 FileReader 對象
             reader.onload = function(e) {
                 audio.src = e.target.result; // 設置音頻播放器的 src 為文件的數據 URL
+                playRate.value = audio.playbackRate;
             };
             reader.readAsDataURL(file); // 讀取文件並轉換為數據 URL
         }
     });
+
+    playRate.addEventListener('change', () => {
+      audio.playbackRate = playRate.value;
+    })
 });
 
 function formatTime(seconds) {
